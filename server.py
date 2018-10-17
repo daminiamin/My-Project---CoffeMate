73a8f4d502a6c1f3b894e38be49966ecbe8fc159
vagrant <vagrant@vagrant.vm>
2018-10-17 04:26:29 +0000

    Completed like/unlike-dislike/undislike buttons also records on DB

--- server.py
+++ server.py
@@ -109,23 +109,17 @@ def homepage():
     """Show  homepage page"""
 
     user_id = session["user_id"]
-    user_info = User.query.get(user_id)
+    user_info = User.query.get(user_id) #current user info
     # print(f"user_info {user_info}")
 
-    get_likes = user_info.likes # list of all users that the current user likes.
+    get_likes = user_info.likes #list of all users liked by current user.
     liked_ids = set([like.liked_user_id for like in get_likes])
-    #get likes user,liked by current user
 
-    get_dislikes = user_info.dislikes
+    get_dislikes = user_info.dislikes #list of all users disliked by current user.
     disliked_ids = set([dislike.disliked_user_id for dislike in get_dislikes])
-    #get dislikes user,liked by current user
-
+    
+    # adding likes_dislikes of current user 
     excluded_ids = (liked_ids) | (disliked_ids) | set([user_id])
-    # adding likes_dislikes of current user
-
-    # suggestions = (interesting_user_ids - likes_dislikes) - set([user_id])
-    # substracting likes/dislikes,current user ids with same gender people
-    # print(f"suggestions {suggestions}")
 
     # build a list of hobbie ids for current user
     current_user_hobbies = [hobbie.hobbie_id for hobbie in user_info.hobbies]
@@ -134,38 +128,15 @@ def homepage():
     all_interest_users = User.query.join(User_Hobbies).filter((User.gender == user_info.interested_in),
                                 User_Hobbies.hobbie_id.in_(current_user_hobbies),
                                 User.user_id.notin_(list(excluded_ids))).all()
-
-    #macthing interested_in of current user 
-                # to show all users who is that gender only
-                    #get users with cross  M/F connections
-
-    # interesting_user_ids = set([user.user_id for user in all_interest_users ])
-    # store all user id's of that all same gender users
-
-
-    # get_allobjects = db.session.query(User).filter(User.user_id.in_(suggestions)).all()
-    # getting all users object using all suggestion id
-    
-
-    # get current user's hobbies
-    # get_same_hobbie_users=db.session.query(User).filter(User.hobbie_id.in_(current_user_hobbies)).all()
-    # getting users who has same hobbie as current user
-
+    #macthing interested_in current user 
+                # show all users who has common hobbies 
+                    #get users not in excluded_ids(likes,dislikes,c.Uid)
 
 
     if len(all_interest_users) > 3:
 
         all_interest_users = sample(all_interest_users, 3)
         #get3 users who has same hobbie
-
-
-    # if button like:
-    #     user.add.current user
-    # elif dislikes:
-    #     user.add.c
-
-    #user clicks on heart it should added into her/his list
-    #user clicks on dislikes  it should remove that person from suggestion temparary
 
     return render_template("homepage.html", user_info=user_info,
                                             get_allobjects=all_interest_users)
@@ -175,11 +146,11 @@ def homepage():
 def profile(user_id):
     """Show the user profile for that user."""
 
-    user_info = User.query.get(user_id)
+    another_user_info = User.query.get(user_id)
 
     # user_info = User.query.filter_by(user_id=user_id).first()
 
-    return render_template("profile.html", user_info=user_info)
+    return render_template("profile.html", user_info=another_user_info)
 
 
 @app.route('/logout')
@@ -196,42 +167,101 @@ def logout():
     #     session['user_id'] = None
     # return user
 
-
-@app.route('/like')
+############## LIKE ############################
+@app.route('/like', methods=['POST'])
 def like():
     """ liking user by saving into database """
 
-    user_id = session["user_id"]
+    user_id = session["user_id"]#current user id
 
-    liked = request.form['liked']
+    liked_user_id = int(request.form.get('likedUserId'))#liked user id
 
-    liked_user = Like(likes_user_id=int(user_id),liked_user_id =int(liked))
+    # query Like table to see if this user has already liked the person
+    liked_in_like = Like.query.filter_by(likes_user_id=user_id , liked_user_id=liked_user_id).first()
+
+    if not liked_in_like:
+        # adding liked user to like table
+        add_liked_user = Like(likes_user_id=user_id,liked_user_id=liked_user_id)
+        db.session.add(add_liked_user)
+
+    # check liked user exist in dislike table DB
+    liked_in_dislike = Dislike.query.filter_by(dislikes_user_id=user_id , disliked_user_id=liked_user_id).first()
+
+    if liked_in_dislike:
+        # delete record from dislike table
+        db.session.delete(liked_in_dislike)
     
-    db.session.add(liked_user)
     db.session.commit()
 
-    return redirect("/homepage")
+    return "OK"
+############## UNLIKE ############################
 
+@app.route('/unlike', methods=['POST'])
+def unlike():
+    """ unliking user by saving into database """
 
-@app.route('/dislike')
+    user_id = session["user_id"]#current user id
+
+    unliked_user_id = int(request.form.get('likedUserId'))#unliked user id
+
+    # check if unliked user in like tbl
+    unliked_in_like =Like.query.filter_by(likes_user_id=user_id, liked_user_id=unliked_user_id).first()
+
+    
+    if unliked_in_like:
+        # delete record from Dislike table
+        db.session.delete(unliked_in_like)
+
+    db.session.commit()
+    return "OK"
+
+############## DISLIKE ############################
+
+@app.route('/dislike', methods=["POST"])
 def dislike():
     """ disliking user by saving into database """
 
+    user_id = session["user_id"]#current user id
 
+    disliked_user_id = int(request.form.get('dislikedUserId')) 
 
-    user_id = session["user_id"]
+    # Check to see if this user already dislikes the person
+    disliked_in_dislike = Dislike.query.filter_by(dislikes_user_id=user_id , disliked_user_id=disliked_user_id).first()
 
-    disliked = request.form['disliked']
+    if not disliked_in_dislike:
+        # adding disliked user to Dislike table
+        add_disliked_user = Dislike(dislikes_user_id=user_id,disliked_user_id=disliked_user_id)
+        db.session.add(add_disliked_user)
 
-    disliked_user = Dislike(dislikes_user_id=int(user_id),disliked_user_id =int(disliked))
-    
-    db.session.add(disliked_user)
+    # check disliked user in likes tbl
+    disliked_in_like = Like.query.filter_by(likes_user_id=user_id, liked_user_id=disliked_user_id).first()
+
+    if disliked_in_like:
+        # delete record from like table
+        db.session.delete(disliked_in_like)
+
     db.session.commit()
+    return "OK"
 
-    return redirect("/homepage")
+############## UNDISLIKE ############################
 
+@app.route('/undislike', methods=['POST'])
+def undislike():
+    """ undisliking user by saving into database """
 
+    user_id = session["user_id"]#current user id
 
+    undisliked_user_id = int(request.form.get('dislikedUserId'))#undisliked user id
+
+    # check if disliked user in dislike tbl
+    undisliked_in_dislike = Dislike.query.filter_by(dislikes_user_id=user_id,disliked_user_id=undisliked_user_id).first()
+ 
+    if undisliked_in_dislike:
+        # delete record from Dislike table
+        db.session.delete(undisliked_in_dislike)
+
+    db.session.commit()
+    return "OK"
 
 ##########Test Route##################
 
